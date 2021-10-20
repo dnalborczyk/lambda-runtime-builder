4c2e419333d3b2463846a49fc15d7a03213d8ea4
Daniel Nalborczyk <dnalborczyk@gmail.com>
2021-10-19 20:08:52 -0400

    fix: add function return value

--- src/downloadFile.ts
+++ src/downloadFile.ts
@@ -6,14 +6,12 @@ const NODE_BUILD_URL = 'https://nodejs.org'
 export default async function downloadFile(
   nodeVersion: string,
   tarArchive: string,
-) {
+): Promise<Response> {
   const url = new URL(
     `dist/v${nodeVersion}/${tarArchive}.tar.gz`,
     NODE_BUILD_URL,
   )
   // console.log(String(url))
-
-  // let response: Response
 
   try {
     return await fetch(String(url))
